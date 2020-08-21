e7d9eabf188f0aac2a1410920ee5977115554fa8
jpolacco <jpolacco@gmail.com>
2020-08-21 13:13:47 -0700

    Add files via upload

---
+++ Pepys.java
@@ -0,0 +1,34 @@
+/**
+ * Which is more likely: at least one 6 in 6 rolls of a fair die,
+ * or at least two 6's in 12 rolls of a fair die?
+ * 
+ */
+public class Pepys
+{
+    /**  
+     * @param n the number of simulations (experiments) to run
+     * (A simulation is rolling a die 6 times.)
+     * @return the estimated likelihood of rolling at least one 6 in 6 rolls of a fair die
+     */
+    public double probAtLeastOneSix(int n){
+        // TODO
+        return 0.0; // delete this line of code
+        
+    }
+    
+     /**
+     * @param n the number of simulations (experiments) to run
+     * (A simulation is rolling a die 12 times.)
+     * @return the estimated likelihood of rolling at least two 6's in 12 rolls of a fair die
+     */
+    public double probAtLeastTwoSixes(int n){
+        // TODO
+        return 0.0; // delete this line of code
+        
+    }
+ 
+}
+        
+        
+            
+            
